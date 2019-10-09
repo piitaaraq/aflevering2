8fef1cfe9a806202440719d99c731815f369b48f
peter.lovstrom@gmail.com <peter.lovstrom@gmail.com>
2019-10-09 14:16:40 +0200

    Færdig program der kører

--- aflevering.c
+++ aflevering.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void opret(float deltagere[][15], int i, int j, char m)
+void opret(float deltagere[][15])
 {
+    int i, j;
+    char m;
     float euro = 7.47;
     printf("Du er i opret funktionen\n");
     // vi bruger et enkelt to-dimensionelt array for at holde koden "ren"
@@ -106,71 +108,142 @@ void opret(float deltagere[][15], int i, int j, char m)
             scanf(" %c", &m);
         };
     };
-
-    printf("Ude af opret modulet. Tryk på en tast OG [Enter] for at "
-           "afslutte.");
-    char afslut;
-    scanf(" %c", &afslut);
-
-    // beregning af hvad differencen er for hver deltager
 }
 // udskriv funktionen (rykkes som ovenstående funktionen)
-void udskriv(void)
+void udskriv(float deltagere[8][15])
 {
-    printf("Du er i udskriv funktionen");
-    float deltagere[8][15] = {
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}};
-    float totalsum, gennemsnit;
-    int i, j, k;
-    char m = 'j';
+    int userInput1 = 1;
+    int j, i;
+    printf("For at udskrive for en bestemt deltager tast: 1\n");
+    printf("For at udskrive alle de registrerede oplysninger: 2\n");
+    printf("For at gå tilbage til hovedmenuen: 0\n\n");
+    printf("Indtast her: ");
+    scanf(" %d", &userInput1);
 
-    opret(deltagere, 15, 15, 'j');
-
-    // visning af alle omkostninger
-    printf("Overblik over alle omkostninger:\n");
-    printf("Deltager\t | Madomkostninger\t | Rejseomkostninger\t | Ophold\t | "
-           "Total\t | Difference\n");
-    for (j = 0; j < 15; j++)
+    switch (userInput1)
     {
-        printf("%03.0f\t\t | DKK%5.2f\t\t | DKK%5.2f\t\t | DKK%5.2f\t | DKK%5.2f\t "
-               "| DKK%5.2f\n",
-               deltagere[0][j], deltagere[1][j], deltagere[3][j], deltagere[5][j],
-               deltagere[6][j], deltagere[7][j]);
-    };
-
-    // beregning af de samlede omkostninger og fordeling
-    totalsum = totalsum + deltagere[6][i];
-    printf("Det totale regnskab indtil videre: %8.2f\n", totalsum);
+    case 1:
+        printf("Indtast brugerID: ");
+        scanf("%d", &i);
+        i = i - 1;
+        printf("Deltager\t Madomkostninger\t Rejseomkostninger\t Ophold\t\t\n ");
+        printf("%03.0f\t\t | DKK%5.2f\t\t | DKK%5.2f\t\t | DKK%5.2f\n", deltagere[0][i], deltagere[1][i], deltagere[3][i], deltagere[5][i]);
+        break;
+    case 2:
+        printf("Deltager\t Madomkostninger\t Rejseomkostninger\t Ophold\t\t\n ");
+        for (j = 0; j < 15; j++)
+        {
+            printf("%03.0f\t\t | DKK%5.2f\t\t | DKK%5.2f\t\t | DKK%5.2f\n",
+                   deltagere[0][j], deltagere[1][j], deltagere[3][j], deltagere[5][j]);
+        };
+        break;
+    case 0:
+        break;
+    }
 }
 // Menu funktionen (Rykkes som ovenstående)
 void menu(void)
 {
     // Definere variable
-    int userInput1;
+    float id = 0;
+    float id1 = 0;
+    int userInput1 = 1;
+    int j;
+    float totalSum = 0, maximum, minimum;
+    int aggr = 0;
     float deltagere[8][15] = {
         {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}};
-    char m = 'j';
 
-    // Selve menuen som printer opret og udskriv delen
-    printf("For at oprette tast: 1\n");
-    printf("For at udskrive tast: 2\n");
-    printf("For at afslutte tast: 0\n");
-
-    // Bruger vælger
-    printf("Indtast her: ");
-    scanf("%d", &userInput1);
-
-    switch (userInput1)
+    while (userInput1 != 0)
     {
-    case 1:
+        // Selve menuen som printer opret og udskriv delen
+        printf("For at indtaste data tast: 1\n");
+        printf("For at udskrive indtastede data tast: 2\n");
+        printf("For at udskrive samlede udgifter tast: 3\n");
+        printf("For at udskrive feriens samlede pris og balance for hver deltager: 4\n");
+        printf("For at se hvilke deltager har haft henholdsvis det største og mindste udlæg til turen: 5\n");
+        printf("For at afslutte tast: 0\n\n");
 
-        opret(deltagere, 15, 15, 'j');
-        break;
-    case 2:
-        udskriv();
-        break;
-    case 0:
-        break;
+        // Bruger vælger
+        printf("Indtast her: ");
+        scanf(" %d", &userInput1);
+
+        switch (userInput1)
+        {
+        case 1:
+
+            opret(deltagere);
+            break;
+        case 2:
+            udskriv(deltagere);
+            break;
+        case 3:
+            printf("Tabellen viser hver udgiftspost og samlede udgifter for alle deltagere.\n");
+            printf("Deltager\t Madomkostninger\t Rejseomkostninger\t Ophold\t\t "
+                   "Total\n");
+            for (j = 0; j < 15; j++)
+            {
+                printf("%03.0f\t\t | DKK%5.2f\t\t | DKK%5.2f\t\t | DKK%5.2f\t | DKK%5.2f\n",
+                       deltagere[0][j], deltagere[1][j], deltagere[3][j], deltagere[5][j], deltagere[6][j]);
+            }
+            break;
+        case 4:
+            //beregning af feriens samlede pris, pris pr deltager og hvorvidt hver deltager
+            //skal betale mere eller have penge i retur. Samlet pris for hver deltager er beregnet i forvejen
+            for (j = 0; j < 15; j++)
+            {
+                totalSum = totalSum + deltagere[6][j];
+            }
+
+            //udskriv feriens samlede pris
+            printf("Den samlede pris for ferien er: DKK%.2f\n", totalSum);
+
+            //beregn gennemsnit for udgifter
+            aggr = totalSum / 15;
+
+            //udfyld den søjle hvor differencen for hver deltager er
+            for (j = 0; j < 15; j++)
+            {
+                deltagere[7][j] = deltagere[6][j] - aggr;
+            }
+
+            //udskriv balance for hver deltager
+            printf("De samlede udgifter er fordelt som nedenstående. Tabellen viser også hvad den enkelte deltager har af penge til gode, eller skal betale til gruppen.\n");
+            printf("Deltager\t | Total\t | Difference\n");
+            for (j = 0; j < 15; j++)
+            {
+                printf("%03.0f\t\t | DKK%5.2f\t | DKK%5.2f\n",
+                       deltagere[0][j], deltagere[6][j], deltagere[7][j]);
+            }
+
+            break;
+        case 5:
+            maximum = deltagere[6][0];
+            minimum = deltagere[6][0];
+            //find hvem der har haft henholdsvis det største og mindste udlæg
+            for (j = 0; j < 15; j++)
+            {
+                if (deltagere[6][j] > maximum)
+                {
+                    maximum = deltagere[6][j];
+                    id = deltagere[0][j];
+                }
+            }
+
+            for (j = 0; j < 15; j++)
+            {
+                if (deltagere[6][j] < minimum)
+                {
+                    minimum = deltagere[6][j];
+                    id1 = deltagere[0][j];
+                }
+            }
+
+            printf("Den deltager der har mindst og dermed skylder en omgang er %03.0f - for %8.2f og den deltager der havde størst udlæg er %03.0f - for %8.2f.\n\n", id1, minimum, id, maximum);
+            break;
+        case 0:
+            break;
+        }
     }
 }
 
